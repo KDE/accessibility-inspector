4551cf2ab44743c826f4f8b38d2efe321df8933c
Laurent Montel <montel@kde.org>
2023-11-16 13:17:46 +0100

    Fix extract messages

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-# SPDX-FileCopyrightText: 2020 Tobias Fella <fella@posteo.de>
 # SPDX-License-Identifier: CC0-1.0
-$XGETTEXT `find -name \*.cpp -o -o $podir/accessibilityinspector.pot
+# SPDX-FileCopyrightText: 2020 Tobias Fella <fella@posteo.de>
+#
+$EXTRACTRC */*.ui >> rc.cpp || exit 11
+$XGETTEXT  `find -name '*.cpp'` -o $podir/accessibilityinspector.pot
+rm -rf rc.cpp
