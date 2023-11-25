85a83dc306d46b4dbbc7c8742e89b61273025a73
Laurent Montel <montel@kde.org>
2023-11-25 14:14:40 +0100

    Prepare to extract propertytreeview

---
+++ src/propertytreeview.cpp
@@ -0,0 +1,22 @@
+/*
+    SPDX-FileCopyrightText: 2023 Montel Laurent <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "propertytreeview.h"
+#include <KLocalizedString>
+
+PropertyTreeView::PropertyTreeView(QWidget *parent)
+    : QTreeView(parent)
+{
+    setAccessibleName(i18nc("@info:whatsthis", "List of properties"));
+    setAccessibleDescription(i18nc("@info:whatsthis", "Displays properties of the selected accessible object"));
+    setRootIsDecorated(false);
+    setItemsExpandable(true);
+    setExpandsOnDoubleClick(false);
+}
+
+PropertyTreeView::~PropertyTreeView() = default;
+
+#include "moc_propertytreeview.cpp"
---
+++ src/propertytreeview.h
@@ -0,0 +1,17 @@
+/*
+    SPDX-FileCopyrightText: 2023 Montel Laurent <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#pragma once
+
+#include <QTreeView>
+
+class PropertyTreeView : public QTreeView
+{
+    Q_OBJECT
+public:
+    explicit PropertyTreeView(QWidget *parent = nullptr);
+    ~PropertyTreeView() override;
+};
