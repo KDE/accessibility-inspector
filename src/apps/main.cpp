fb2e334418b950ed56d2bf83244c1a31b07d5ba0
Laurent Montel <montel@kde.org>
2024-01-01 10:50:37 +0100

    GIT_SILENT: happy new year

--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -1,6 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2012 Frederik Gladhorn <gladhorn@kde.org>
-    SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2023-2024 Laurent Montel <montel.org>
 
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
                      QStringLiteral(INSPECTOR_VERSION_STRING),
                      i18n("Accessibility inspector"),
                      KAboutLicense::LGPL_V2,
-                     i18n("© 2021-2023 Carl Schwan, 2021-2023 KDE Community"));
+                     i18n("© 2021-2023 Carl Schwan, 2021-2024 KDE Community"));
 
     about.addAuthor(i18n("Frederik Gladhorn"), i18n("Original author"), QStringLiteral("gladhorn@kde.org"));
 
--- src/uiwidget.cpp
+++ src/uiwidget.cpp
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2023-2024 Laurent Montel <montel.org>
 
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
